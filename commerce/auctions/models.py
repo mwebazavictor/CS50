2a829f817cbde3e73dab71277214547f217aad24
mwebazavictor <mwebazavictorandrew@gmail.com>
2024-05-11 22:58:21 +0300

    Some changes

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -6,6 +7,26 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
+    # info on each listing
     name = models.CharField(max_length=64, null=False)
     image = models.ImageField()
+
     
+    def __str__(self):
+        return f"{self.name}"
+    
+class Bid(models.Model):
+    placer = models.ManyToManyField(User, related_name="purchasers")
+    price = models.IntegerField(max_length=10, null=False)
+    item = models.ForeignKey(Listing, on_delete=models.PROTECT, related_name="item")
+
+
+    def __str__(self):
+        return f"{self.item} for {self.price}"        
+    
+class comment():
+    commenter =  models.ManyToManyField(User, related_name="commenters" )
+    description = models.CharField(max_length=500, null=False)
+
+    def __str__(self):
+        return f"{self.commenter} says {self.description}"
