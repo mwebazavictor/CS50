9011399bde5df948bfa213974bd8e4a766d56cd3
mwebazavictor <mwebazavictorandrew@gmail.com>
2024-12-01 21:38:46 +0300

    Making the like model

--- project4/network/models.py
+++ project4/network/models.py
@@ -13,3 +13,11 @@ class Post(models.Model):
     def __str__(self):
         return(f"Post is {self.content[:10]}")
 
+class Like(models.Model):
+    post = models.ForeignKey(Post,on_delete = models.CASCADE, related_name= 'likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes",null=False)
+    
+    
+    def __str__(self):
+        number_of_likes = self.post.likes.count()
+        return (f"The post {self.post.content[:10]} by {self.user} has {number_of_likes} {'like' if number_of_likes == 1 else 'likes'}.")
